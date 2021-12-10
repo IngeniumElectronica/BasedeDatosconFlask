1a2443cb836bd12d16e8537ec360963de36973de
Ingenium Electronica - Jonathan Baez <proyectoingeniumelectronica@gmail.com>
2021-12-10 14:00:30 -0600

    Mapeo con Flasky y comandos shell

--- basedatos.py
+++ basedatos.py
@@ -23,3 +23,22 @@ class User(db.Model):
 
     def __repr__(self):
         return"<User %r>" %self.username
+
+"""#Creacion de datos
+db.create_all()
+
+#Insercion de datos
+from basedatos import Role, User
+admin_role=Role(name="Admin")
+mod_role=Role(name="Moderator")
+user_role=Role(name="User")
+user_john=User(username="jhon", role=admin_role)
+user_susan=User(username="susan", role=user_role)
+user_david=User(username="david", role=user_role)
+db.session.add(admin_role)
+db.session.add(mod_role)
+db.session.add(user_role)
+db.session.add(user_john)
+db.session.add(user_susan)
+db.session.add(user_david)
+db.session.commit()"""
